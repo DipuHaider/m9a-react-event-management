7f1e2b95cddb69f40820f4c8580f0cd7babce849
Dipu Haider <dipuhaider@gmail.com>
2023-10-07 15:29:31 +0600

    Basic Folder structure.

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        inter: "Inter, sans-serif", // Adds a new `font-inter` class
+      },
+      colors: {
+        "theme-primary": "#FF444A",
+        "theme-dark": "#0B0B0B",
+        "theme-txt40": "#0B0B0B66",
+        "theme-txt50": "#0B0B0B80",
+        "theme-txt70": "#0B0B0BB2",
+        "theme-btn-green": "#009444",
+        "theme-status-green": "#00C49F",
+      },
+    },
+  },
+  plugins: [require("daisyui")],
+  // daisyui: {
+  //   themes: ["light"],
+  // },
+};

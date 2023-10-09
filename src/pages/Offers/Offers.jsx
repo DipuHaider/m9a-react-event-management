f38f3fda0176679b8c000ebbba0d694fa0ec6dad
Dipu Haider <dipuhaider@gmail.com>
2023-10-09 15:48:01 +0600

    Profile page design

--- src/pages/Offers/Offers.jsx
+++ src/pages/Offers/Offers.jsx
@@ -14,22 +14,6 @@ const Offers = () => {
                 {
                     offers.map(singleOffer => <SingleOffer key={singleOffer.id} offers={singleOffer}></SingleOffer>)
                 }
-                
-
-
-                    {/* <div className="bg-white rounded-2xl shadow-xl px-8 py-12 sm:px-12 lg:px-8">
-                        <div className="mb-12 space-y-4">
-                            <h3 className="text-2xl font-semibold text-purple-900">Graphic Design</h3>
-                            <p className="mb-6">Obcaecati, quam? Eligendi, nulla numquam natus laborum porro at cum, consectetur ullam tempora ipsa iste officia sed officiis! Incidunt ea animi officiis.</p>
-                            <a href="#" className="block font-medium text-purple-600">Know more</a>
-                        </div>
-                        <img src="https://tailus.io/sources/blocks/end-image/preview/images/graphic.svg" className="w-2/3 ml-auto -mb-12" alt="illustration" loading="lazy" width="900" height="600"></img>
-                    </div> */}
-
-
-
-
-                    
                 </div>
             </div>
         </div>
--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -1,8 +1,34 @@
+import { useContext } from "react";
+import { AuthContext } from "../../providers/AuthProvider";
+
 const Profile = () => {
+
+    const {user} = useContext(AuthContext);
+    console.log(user)
+
     return (
-        <div className="max-w-6xl mx-auto my-20">
-            <h2>Profile Page</h2>
+
+        <div className="py-32 bg-purple-200 min-h-fit">  
+            <div className="container m-auto px-6 text-gray-500 md:px-12 xl:px-0">
+                <div className="mx-auto grid gap-6 md:w-3/4 lg:w-full lg:grid-cols-1">
+                    <div className="max-w-lg mx-auto my-10 bg-white rounded-lg shadow-md p-5">
+                        <img className="w-32 h-32 rounded-full mx-auto" src={user.photoURL} alt="Profile picture"/>
+                        <h2 className="text-center text-2xl font-semibold mt-3">{user.displayName}</h2>
+                        <p className="text-center text-gray-600 mt-1">{user.email}</p>
+                        <div className="flex justify-center mt-5">
+                        <a href="#" className="text-blue-500 hover:text-blue-700 mx-3">Twitter</a>
+                        <a href="#" className="text-blue-500 hover:text-blue-700 mx-3">LinkedIn</a>
+                        <a href="#" className="text-blue-500 hover:text-blue-700 mx-3">GitHub</a>
+                        </div>
+                        <div className="mt-5">
+                        <h3 className="text-xl font-semibold">Bio</h3>
+                        <p className="text-gray-600 mt-2">Hello!, My Name is {user.displayName}. Tech enthusiast, coding wizard, and coffee lover ☕. Exploring the digital frontier, one line of code at a time. you can reach me at {user.email}</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
+        
     );
 };
 
